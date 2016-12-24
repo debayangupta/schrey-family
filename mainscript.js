5d8fba460a0f63309f8482a7319a7e6bb96402ea
Debayan Gupta <debayan.gupta@yale.edu>
2016-12-24 02:24:17 -0500

    checking

--- mainscript.js
+++ mainscript.js
@@ -22,7 +22,8 @@ $(function() {
   });
   $(document).trigger('scroll'); // in case someone opens a # directly
   $('#navbar>div>a').each(function(){
-  	if(document.location == this.href || document.location+'index.html' == this.href) {
+  	console.log(document.location+'=='+this.href);
+  	if(document.location == this.href) {
 	  	$(this).addClass('current');
 	  	$('header').css("background-image","url('images/"+this.innerHTML+".jpg')");
 	  }
